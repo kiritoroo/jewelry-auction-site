36e6eccf9e2be99a4b29e2a7a046f6f20dbb509f
Duy Hung <dongocduyhung7@gmail.com>
2022-12-17 22:14:55 +0700

    [221217] - cart style

--- frontend/src/components/CartManager/CartManager.tsx
+++ frontend/src/components/CartManager/CartManager.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useAuthContext } from "@context/AuthContext";
 
 import * as S from "./CartManager.styled";
@@ -12,7 +11,35 @@ export const CartManager = (props: Props) => {
 
   return (
     <>
-      <S.Wrapper>Cart page</S.Wrapper>
+      <S.Wrapper>
+        Your Cart
+        <S.container>
+          <S.cart>
+            <S.cartImage>Hinhf anhr</S.cartImage>
+            <S.cartName>Name: kim cuong</S.cartName>
+            <S.cartAmount>
+              Amount:
+              <S.cartAmountInput type="number" min="1"></S.cartAmountInput>
+            </S.cartAmount>
+            <S.cartPrice>Price: 100000</S.cartPrice>
+            <S.btnDelete>Delete</S.btnDelete>
+          </S.cart>
+        </S.container>
+        {/* 2 */}
+        <S.container>
+          <S.cart>
+            <S.cartImage>Hinhf anhr</S.cartImage>
+            <S.cartName>Name: kim cuong</S.cartName>
+            <S.cartAmount>
+              Amount:
+              <S.cartAmountInput type="number" min="1"></S.cartAmountInput>
+            </S.cartAmount>
+            <S.cartPrice>Price: 100000</S.cartPrice>
+            <S.btnDelete>Delete</S.btnDelete>
+          </S.cart>
+        </S.container>
+        <S.btnBuy>Payment</S.btnBuy>
+      </S.Wrapper>
     </>
   );
 };
